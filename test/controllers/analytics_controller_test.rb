19bd9879089698f58e094100bb5d2fc53a2aa585
Andreas Polycarpou <ahpolycarpou@gmail.com>
2015-09-04 13:29:39 -0600

    modified views

--- test/controllers/analytics_controller_test.rb
+++ test/controllers/analytics_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
-class AnalyticsControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "iTriage Analytics Pages"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | iTriage Analytics Pages"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | iTriage Analytics Pages"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | iTriage Analytics Pages"
+    assert_select "title", "About | #{@base_title}"
   end
-
 end
